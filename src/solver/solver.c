015b342a8f412a233b4619731954b4c454d84229
Océane Delogé <oceanedeloge@Oceanes-MacBook-Air.local>
2025-10-26 12:29:33 +0100

    solver fix

--- src/solver/solver.c
+++ src/solver/solver.c
@@ -30,19 +30,19 @@ void read_grid(const char *filename) {
     fclose(file);
 }
 
-int check_word_in_direction(char word[], int x, int y, int dx, int dy) 
+int check_word_in_direction(char word[], int x, int y, int x1, int y1) 
 {
     int len = strlen(word);
     for (int i = 0; i < len; i++) 
     {
-        int x1 = x + i * dx;
-        int y1 = y + i * dy;
+        int x2 = x + i * x1;
+        int y2 = y + i * y1;
 
-        if (x1 < 0 || x1 >= rows || y1 < 0 || y1 >= cols)
+        if (x2 < 0 || x2 >= rows || y2 < 0 || y2 >= cols)
         {
             return 0;
         }
-        if (grid[x1][y1] != word[i])
+        if (grid[x2][y2] != word[i])
         {    
              return 0;
         }
@@ -50,22 +50,26 @@ int check_word_in_direction(char word[], int x, int y, int dx, int dy)
     return 1;
 }
 
-int find_word(char word[], int *x0, int *y0, int *x1, int *y1) 
+int find_word(char word[], int *x, int *y, int *x1, int *y1) 
 {
     int directions[8][2] = {
         {0, 1}, {0, -1}, {1, 0}, {-1, 0},
         {1, 1}, {1, -1}, {-1, 1}, {-1, -1}
     };
 
-    for (int x = 0; x < rows; x++) {
-        for (int y = 0; y < cols; y++) {
-            if (grid[x][y] == word[0]) {
-                for (int d = 0; d < 8; d++) {
+    for (int x2 = 0; x2 < rows; x++) 
+    {
+        for (int y2 = 0; y2 < cols; y++) 
+        {
+            if (grid[x2][y2] == word[0]) 
+            {
+                for (int d = 0; d < 8; d++) 
+                {
                     if (check_word_in_direction(word, x, y, directions[d][0], directions[d][1])) {
-                        *x0 = y;
-                        *y0 = x;
-                        *x1 = y + (strlen(word) - 1) * directions[d][1];
-                        *y1 = x + (strlen(word) - 1) * directions[d][0];
+                        *x = y2;
+                        *y = x2;
+                        *x1 = y2 + (strlen(word) - 1) * directions[d][1];
+                        *y1 = x2 + (strlen(word) - 1) * directions[d][0];
                         return 1;
                     }
                 }
